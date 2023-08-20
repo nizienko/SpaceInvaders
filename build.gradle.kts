cb45546ceb554ed17807da010d074291ac5f0c36
nizienko <eugene.nizienko@gmail.com>
2023-08-20 23:06:48 +0200

    gradle updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.7.21"
-    id("org.jetbrains.intellij") version "1.10.1"
+    id("org.jetbrains.intellij") version "1.15.0"
 }
 
 group = "com.github.nizienko"
