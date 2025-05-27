cf3a5e688447a99867ff8b5b04f306fcba53c2d1
nizienko <eugene.nizienko@gmail.com>
2025-05-27 22:32:17 +0200

    updated supported version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.nizienko"
-version = "1.1.2"
+version = "1.1.3"
 
 repositories {
     mavenCentral()
@@ -31,7 +31,7 @@ intellijPlatform {
     pluginConfiguration {
         ideaVersion {
             sinceBuild.set("241.1")
-            untilBuild.set("251.*")
+            untilBuild.set("252.*")
         }
     }
 }
